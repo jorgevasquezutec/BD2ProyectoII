0b132c5c334ba962ab983be9eacea0d67c338282
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2020-11-20 01:46:08 -0500

    iterate in json

--- prueba/prueba.py
+++ prueba/prueba.py
@@ -1,28 +1,12 @@
-# from itertools import chain
-# from nltk import FreqDist, sent_tokenize, word_tokenize # $ pip install nltk
-# import linecache
+# import json
+# with open('prueba/tweets_2018-08-07.json') as infile:
+#   o = json.load(infile)
+#   chunkSize = 1000
+#   for i in range(0, len(o), chunkSize):
+#     with open('file_' + str(i//chunkSize) + '.json', 'w') as outfile:
+#       json.dump(o[i:i+chunkSize], outfile)
 
-# # with open('prueba/input.txt') as file:
-# #     text = file.read()
-# # words = chain.from_iterable(map(word_tokenize, sent_tokenize(text)))
-# # freq = FreqDist(map(str.casefold, words))
-# # freq.pprint()
-# filename='prueba/input.txt'
-# line = linecache.getline(filename, 2)
-# print(line)
 
 import pandas as pd
-
-df=pd.read_json("out2/0tf_df.json",  nrows = 10, lines = True)
-print(df)
-# self.orderedList = (pd.read_csv(tableName,skiprows = self.iteration, nrows = self.sizeOfChunk)).values.tolist()
-#{ 107 : {DocA : freq}, {DocB: freq}
-#
-#
-#}
-
-#{
-#   {107 : freq}
-#   {102 : freq}
-#   {103 : freq}
-#}
+for index,chunk in enumerate(pd.read_json('0tf_df.txt', lines=True, chunksize=5),start=1):
+    print(chunk)
